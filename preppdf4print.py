bc8778b55d026a53b5e92f1135421dbd8cc3140e
Sam Thomas <sam@vector.ai>
2020-11-28 18:48:50 +0000

    Move from NB -> script

--- preppdf4print.py
+++ preppdf4print.py
@@ -13,44 +13,20 @@ from pdf2image import convert_from_path
 
 ## Setup args & constants
 THIS_FILE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-argparser = argparse.ArgumentParser()
-argparser.add_argument('--input-pdf', required=True)
-argparser.add_argument('--dpi', type=int, default=72)
-argparser.add_argument('--front-cover', type=int, default=None)
-argparser.add_argument('--back-cover', type=int, default=None)
-argparser.add_argument('--content-range', type=int, nargs=2, default=None)
-argparser.add_argument('--poppler-path', help='Path to poppler/bin', default=None)
-
-args = argparser.parse_args()
-
-INPUT_PDF = args.input_pdf
-
-USU_PER_IN = args.dpi
-IN_TO_MM = 25.4
-USU_PER_MM = USU_PER_IN / IN_TO_MM
-
-FRONT_COVER_PAGE = args.front_cover
-BACK_COVER_PAGE = args.back_cover
-CONTENT_RANGE = (1, PdfFileReader(INPUT_PDF).getNumPages()) if args.content_range is None else args.content_range
-N_PAGES = CONTENT_RANGE[1]-CONTENT_RANGE[0]+1
-
-POPPLER_PATH = args.poppler_path
-
 with open(os.path.join(THIS_FILE_DIR, './paper_sizes.json')) as f: PAPER_SIZES = json.load(f)
 with open(os.path.join(THIS_FILE_DIR, './gutter_sizes.json')) as f: GUTTER_SIZES = {int(kk): vv for kk, vv in json.load(f).items()}
 
-def generate_document_coverage_array(pdf_path, page_range, sample_period=1, dpi=72, poppler_path=None):
-    arr_overall = None
-    for pp in range(*page_range, sample_period):
-        print(f'\r{pp/page_range[1]*100:.0f}%...', end='')
-        im = convert_from_path(pdf_path, dpi=dpi, first_page=pp, last_page=pp, poppler_path=poppler_path)[0]
-        im_arr = np.array(im)
-        if arr_overall is None: arr_overall = np.full(im_arr.shape[:2], False)
-        arr_overall += (im_arr != 255).all(axis=2)
-    print('done')
+def generate_document_coverage_array(pdf_path, page_range=None, sample_period=1, dpi=72, poppler_path=None):
+    if page_range is None: page_range = (1, PdfFileReader(pdf_path).getNumPages())
 
-    return arr_overall
+    coverage_arr = None
+    for pp in range(page_range[0], page_range[1]+1, sample_period):
+        im = convert_from_path(pdf_path, first_page=pp, last_page=pp, dpi=dpi, poppler_path=poppler_path)[0]
+        im_arr = np.array(im)
+        if coverage_arr is None: coverage_arr = np.full(im_arr.shape[:2], False)
+        coverage_arr += (im_arr != 255).all(axis=2)
+
+    return coverage_arr
 
 def find_margins_px(page_array, pad=(0,0,0,0)):
     pad_l, pad_r, pad_t, pad_b = pad
@@ -112,7 +88,9 @@ def draw_cross(draw_obj, centre, length, thickness, colour):
 
     return draw_obj
 
-def annotate_found_margins(page_array, margin_l, margin_r, margin_t, margin_b):
+def annotate_found_margins(page_array, margins):
+    margin_l, margin_r, margin_t, margin_b = margins
+
     im = Image.fromarray(page_array)
     im_annot = im.copy().convert('RGB')
     draw = ImageDraw.Draw(im_annot)
@@ -124,19 +102,161 @@ def annotate_found_margins(page_array, margin_l, margin_r, margin_t, margin_b):
     draw = draw_cross(draw, content_centroid_pil, 5, 2, 'red')
 
     page_centroid_pil = midpoint((0,0), im.size, round_coords=True)
-    draw = draw_cross(draw, page_centroid_pil, 5, 2, 'blue')
+    draw = draw_cross(draw, page_centroid_pil, 5, 2, 'green')
 
     return im_annot
 
-TEST_PAGE = 26
+def user_adjust_margins(page_array, orginal_margins):
+    margins = orginal_margins
+    padding = [0, 0, 0, 0]
 
-# arr_overall = generate_document_coverage_array(INPUT_PDF, *CONTENT_RANGE, 10, USU_PER_IN, POPPLER_PATH)
-arr_overall = generate_document_coverage_array(INPUT_PDF, [TEST_PAGE, TEST_PAGE+1], 10, USU_PER_IN, POPPLER_PATH)
-margin_l, margin_r, margin_t, margin_b = find_margins_px(arr_overall)
-# margin_l, margin_r, margin_t, margin_b = find_margins_px(arr_overall, pad_l=33)
+    redraw = True
+    while True:
+        if redraw:
+            print(f'L/R/T/B: {margins}')
+            page_im_annot = annotate_found_margins(page_array, margins)
+            page_im_annot.show()
 
-print(f'L: {margin_l}, R: {margin_r}, T: {margin_t}, B: {margin_b}')
+        userinput = input('Adjust margins?: ').lower().strip()
+        if userinput == '':
+            break
 
-im_annot = annotate_found_margins(arr_overall, margin_l, margin_r, margin_t, margin_b)
-im_annot.show()
-print('asdsa')
+        elif len(userinput) < 3 or not (userinput[0] in 'lrtb' and userinput[1] in '-+' and userinput[2:].isdigit()):
+            print('Invalid input, must be comma-separated groups of [lrtb][+-][0-9]+ (or "DONE" when done)')
+            redraw = False
+
+        else:
+            delta = int(userinput[2:]) * (-1 if userinput[1] == '-' else 1)
+            padding['lrtb'.index(userinput[0])] += delta
+            margins = tuple(sum(ii) for ii in zip(orginal_margins, padding))
+            redraw = True
+
+    return margins, padding
+
+def calculate_content_scale_factor(content_size_px, target_paper_size_mm, target_margin_x_mm, target_margin_y_mm, gutter_mm):
+    target_content_size_mm = target_paper_size_mm[0]-2*target_margin_x_mm-gutter_mm, target_paper_size_mm[1]-2*target_margin_y_mm
+
+    content_size_mm = content_size_px[0] / USU_PER_MM, content_size_px[1] / USU_PER_MM
+    content_delta_x, content_delta_y = target_content_size_mm[0] - content_size_mm[0], target_content_size_mm[1] - content_size_mm[1]
+    content_scale_x, content_scale_y = 1+content_delta_x/content_size_mm[0], 1+content_delta_y/content_size_mm[1]
+    content_scale_factor = min(content_scale_x, content_scale_y)
+
+    return content_scale_factor
+
+def get_content_size_and_translation(source_page_mediabox, content_margins):
+    content_ll = (source_page_mediabox.lowerLeft[0]+content_margins[0], source_page_mediabox.lowerLeft[1]+content_margins[3])
+    content_ur = (source_page_mediabox.upperRight[0]-content_margins[1], source_page_mediabox.upperRight[1]-content_margins[2])
+    content_size = (content_ur[0]-content_ll[0], content_ur[1]-content_ll[1])
+
+    content_centroid = midpoint(content_ll, content_ur)
+    page_centroid = midpoint((0,0), source_page_mediabox.upperRight)
+    content_tx = round(page_centroid[0]-content_centroid[0]), round(page_centroid[1]-content_centroid[1]) # translation FROM content centroid TO page centroid
+
+    return content_size, content_tx
+
+def translate_and_scale_onto_target_paper(source_page, content_tx, content_scale_factor, target_paper_size_px):
+    src_page_trans_scale = PageObject.createBlankPage(width=source_page.mediaBox.getWidth(), height=source_page.mediaBox.getHeight())
+    src_page_trans_scale.mergeTranslatedPage(source_page, tx=content_tx[0], ty=content_tx[1])
+    src_page_trans_scale.scaleBy(content_scale_factor)
+
+    rescale_page_on_tgt_paper = PageObject.createBlankPage(width=target_paper_size_px[0], height=target_paper_size_px[1])
+    rescale_page_on_tgt_paper.mergePage(src_page_trans_scale)
+
+    return rescale_page_on_tgt_paper
+
+def parse_cli_args():
+    global USU_PER_MM
+
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('--input-pdf', required=True)
+    argparser.add_argument('--output-pdf', default=None)
+    argparser.add_argument('--paper-type', choices=PAPER_SIZES.keys(), default=None)
+    argparser.add_argument('--margin_x', type=int, default=13, help='X margin w/o gutter (in mm)')
+    argparser.add_argument('--margin_y', type=int, default=13, help='Y margin w/o gutter (in mm)')
+    argparser.add_argument('--gutter', type=int, default=None, help='Gutter in mm')
+    argparser.add_argument('--no-rescale', action='store_true')
+    argparser.add_argument('--dpi', type=int, default=72)
+    argparser.add_argument('--coverage-sample-range', type=int, nargs=2, default=None)
+    argparser.add_argument('--coverage-sample-period', type=int, default=1)
+    argparser.add_argument('--content-range', type=int, nargs=2, default=None)
+    argparser.add_argument('--poppler-path', help='Path to poppler/bin', default=None)
+
+    args = argparser.parse_args()
+
+    if not os.path.isfile(args.input_pdf): raise Exception(f'Not a file: {args.input_pdf}')
+
+    USU_PER_MM = args.dpi / 25.4
+
+    if args.content_range is None: args.content_range = (1, PdfFileReader(args.input_pdf).getNumPages())
+    if args.coverage_sample_range is None: args.coverage_sample_range = args.content_range
+
+    return args
+
+if __name__ == '__main__':
+    args = parse_cli_args()
+    n_pages = args.content_range[1]-args.content_range[0]+1
+    gutter_mm = find_gutter_size_mm_from_pages(n_pages) if args.gutter is None else args.gutter
+
+    pdf_reader = PdfFileReader(args.input_pdf)
+
+    coverage_arr = generate_document_coverage_array(args.input_pdf, args.coverage_sample_range, args.coverage_sample_period, args.dpi, poppler_path=args.poppler_path)
+    content_margins = find_margins_px(coverage_arr)
+    content_margins, content_margins_padding = user_adjust_margins(coverage_arr, content_margins)
+
+    content_size, content_tx = get_content_size_and_translation(pdf_reader.getPage(args.content_range[0]-1).mediaBox, content_margins)
+
+    if args.paper_type is None:
+        target_paper_type = find_nearest_paper_by_aspect(*content_size, subtract_margins=(args.margin_x, args.margin_x, args.margin_y, args.margin_y), subtract_gutter=gutter_mm)[0]
+        print(f'Closest paper type by aspect ratio: {target_paper_type}')
+    else:
+        target_paper_type = args.paper_type
+        print(f'Use specified paper type: {target_paper_type}')
+
+    target_paper_size_mm = PAPER_SIZES[target_paper_type]['trim']['x'], PAPER_SIZES[target_paper_type]['trim']['y']
+    target_paper_size_px = (round(target_paper_size_mm[0]*USU_PER_MM), round(target_paper_size_mm[1]*USU_PER_MM))
+
+    if not args.no_rescale:
+        content_scale_factor = calculate_content_scale_factor(content_size, target_paper_size_mm, args.margin_x, args.margin_y, gutter_mm)
+    print(f'Content scale factor: {content_scale_factor}{" (overridden)" if args.no_rescale else ""}')
+
+    transformed_pages = []
+    for ii, page_num in enumerate(range(args.content_range[0], args.content_range[1]+1)):
+        print(f'\rTransforming page {ii} ({ii/(args.content_range[1]+1-args.content_range[0])*100:.1f}%)...', end='')
+        src_page = pdf_reader.getPage(page_num-1)
+        rescale_page_on_tgt_paper = translate_and_scale_onto_target_paper(src_page, content_tx, content_scale_factor, target_paper_size_px)
+        transformed_pages.append(rescale_page_on_tgt_paper)
+    print('done')
+
+    temp_transformed_path = './temp_transformed.pdf'
+    pdf_out = PdfFileWriter()
+    for ii in transformed_pages: pdf_out.addPage(ii)
+    with open(temp_transformed_path, 'wb') as f: pdf_out.write(f)
+
+    coverage_range_shifted = [ii-(args.content_range[0]-1) for ii in args.coverage_sample_range]
+    transformed_coverage_arr = generate_document_coverage_array(temp_transformed_path, coverage_range_shifted, args.coverage_sample_period, args.dpi, poppler_path=args.poppler_path)
+    content_margins_on_tgt_paper = find_margins_px(transformed_coverage_arr, pad=[round(ii*content_scale_factor) for ii in content_margins_padding])
+    content_margins_on_tgt_paper, _ = user_adjust_margins(transformed_coverage_arr, content_margins_on_tgt_paper)
+
+    os.remove(temp_transformed_path)
+
+    _, recentre_content_tx = get_content_size_and_translation(transformed_pages[0].mediaBox, content_margins_on_tgt_paper)
+
+    pdf_out = PdfFileWriter()
+    for ii, page_transformed in enumerate(transformed_pages):
+        print(f'\rRecentering page {ii} ({ii/len(transformed_pages)*100:.1f}%)...', end='')
+        gutter_tx = (gutter_mm * USU_PER_MM * (1 if (ii+1) % 2 else -1)) / 2 # +ve (rightward) shift for odd pages, -ve (left) for even
+
+        page_refit = PageObject.createBlankPage(width=target_paper_size_px[0], height=target_paper_size_px[1])
+        page_refit.mergeTranslatedPage(page_transformed, tx=recentre_content_tx[0]+gutter_tx, ty=recentre_content_tx[1])
+
+        pdf_out.addPage(page_refit)
+    print('done')
+
+    if args.output_pdf is None:
+        output_path = os.path.splitext(args.input_pdf)[0]+f'_refit_{target_paper_type.replace("-", "_")}.pdf'
+    else:
+        output_path = args.output_pdf
+
+    with open(output_path, 'wb') as f: pdf_out.write(f)
+
+    print(f'Wrote output to {output_path}')
